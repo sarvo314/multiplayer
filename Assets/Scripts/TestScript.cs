39f44419e27aef6144fdb98b7e62073e4d3bb7fc
Sarvottam <sarvottamswaroop100@gmail.com>
2023-09-04 01:55:53 +0530

    added player input component

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public void Jump()
+    {
+        Debug.Log("Jump is pressed");
+    }
+
+}
